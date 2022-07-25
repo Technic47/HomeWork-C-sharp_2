95b5dec2693f6753ffef34bb656e9f0bdfef3edf
Pavel Kuznetsov <Technic47@gmail.com>
2022-07-25 13:54:54 +0300

    Test10_V1

---
+++ Tests/Program.cs
@@ -0,0 +1,21 @@
+﻿int number = 12345;
+int[] array = {0,0,0,0,0};
+
+int size = number % 10;
+int index = 0;
+while (size > 1)
+{
+    size = number % 10;
+    number = number / 10;
+    array[index] = size;
+    //Console.Write($"{array[index]} ");
+    index++;
+    //Console.Write($"{size} ");
+    //Console.Write($"{number} ");
+    
+}
+Console.WriteLine("Second number is: ");
+Console.WriteLine(array[1]);
+   
+
+
