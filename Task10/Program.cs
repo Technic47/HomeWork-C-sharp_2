6863f7e4d1f2b8134efd000b4350450188cca139
Pavel Kuznetsov <Technic47@gmail.com>
2022-07-25 15:32:30 +0300

    remarks

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,4 +1,5 @@
-﻿int number = 0;
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int number = 0;
 
 Console.WriteLine("Enter number: ");
 string userenter = Console.ReadLine();
@@ -9,33 +10,27 @@ if (enter)
     int count = 0;
     int number1 = number;
     int ost = 1;
-    while (ost != 0)
+    while (ost != 0) //count is Length of future array
     {
-        //Console.Write($"{number1} ");
         number1 = number1 / 10;
         ost = number1 % 10;
         count++;
-        //Console.WriteLine(count);
     }
 
     int index = 0;
-    Console.WriteLine($"Count is: {count}");
+    //Console.WriteLine($"Count is: {count}");
     int[] array = new int[count];
 
-    while (index < array.Length)
+    while (index < array.Length) //putting ost to array[index]
     {
-        //Console.Write($"{number} ");
         ost = number % 10;
         number = number / 10;
-        
-        //Console.Write(ost);
         array[index] = ost;
-        Console.Write(array[index]);
         index++;
     }
 
 Console.WriteLine("Second number is: ");
-Console.WriteLine(array[array.Length - 2]);
+Console.WriteLine(array[array.Length - 2]); //array[-2] doesn`t work((
     
 }
 if (!enter)
