c475e228a25a28db9b3c2ee271f9d6767e8a033a
Pavel Kuznetsov <Technic47@gmail.com>
2022-07-25 20:12:39 +0300

    remarks

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,5 +1,4 @@
-﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-int number = 0;
+﻿int number = 0;
 
 Console.WriteLine("Enter number: ");
 string userenter = Console.ReadLine();
@@ -10,32 +9,28 @@ if (enter)
     int count = 0;
     int number1 = number;
     int ost = 1;
-    while (ost != 0) //count is Length of future array
+    while (ost != 0) //count is for array.Length
     {
         number1 = number1 / 10;
         ost = number1 % 10;
         count++;
     }
 
-    if (count < 3)
-    {
-        Console.WriteLine("There is no 3rd digit in your number!");
-    }
-    else
-    {
-        int[] array = new int[count];
+    int index = 0;
+    //Console.WriteLine($"Count is: {count}");
+    int[] array = new int[count];
 
-        for (int i = 0; i < array.Length; i++) //putting ost to array[index]
-        {
-            ost = number % 10;
-            number = number / 10;
-            array[i] = ost;
-        }
-        Console.WriteLine("Third number is: ");
-        Console.WriteLine(array[array.Length - 3]); //array[-3] doesn`t work((
-
+    while (index < array.Length)
+    {
+        ost = number % 10;
+        number = number / 10;
+        array[index] = ost;
+        index++;
     }
-        
+
+Console.WriteLine("Second number is: ");
+Console.WriteLine(array[array.Length - 2]);
+    
 }
 if (!enter)
 {
