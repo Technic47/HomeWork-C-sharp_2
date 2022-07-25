e2e328eea164e43941384c5a7e25602db78a4bc8
Pavel Kuznetsov <Technic47@gmail.com>
2022-07-25 15:42:33 +0300

    Task13 done

---
+++ Task13/Program.cs
@@ -0,0 +1,46 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+int number = 0;
+
+Console.WriteLine("Enter number: ");
+string userenter = Console.ReadLine();
+
+bool enter = int.TryParse(userenter, out number);
+if (enter)
+{
+    int count = 0;
+    int number1 = number;
+    int ost = 1;
+    while (ost != 0) //count is Length of future array
+    {
+        number1 = number1 / 10;
+        ost = number1 % 10;
+        count++;
+    }
+
+    if (count < 3)
+    {
+        Console.WriteLine("There is no 3rd digit in your number!");
+    }
+    else
+    {
+        int index = 0;
+        //Console.WriteLine($"Count is: {count}");
+        int[] array = new int[count];
+
+        while (index < array.Length) //putting ost to array[index]
+        {
+            ost = number % 10;
+            number = number / 10;
+            array[index] = ost;
+            index++;
+        }
+        Console.WriteLine("Third number is: ");
+        Console.WriteLine(array[array.Length - 3]); //array[-3] doesn`t work((
+
+    }
+        
+}
+if (!enter)
+{
+    Console.Write("Wrong input");
+}
